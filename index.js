0145e5019ee3c480773e14ed3deb062649133684
Rane Gray <ranegray830@gmail.com>
2023-02-25 01:03:09 +0100

    test

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
-const calcDisplay = document.querySelector('#calc-display');
-const clearBtn = document.querySelector('.clear');
-const digits = document.querySelectorAll('.digit');
-const operators = document.querySelectorAll('.operator');
-const equals = document.querySelector('.equal');
-const decimal = document.querySelector('.decimal');
+/* eslint-disable default-case */
+const calcDisplay = document.querySelector("#calc-display");
+const clearBtn = document.querySelector(".clear");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equal");
+const decimal = document.querySelector(".decimal");
 let num1 = null;
 let num2 = null;
 let operator = null;
 let shouldResetDisplay = true;
 
-const add = (n1, n2) => Number(n1) + Number(n2);
-const subtract = (n1, n2) => n1 - n2;
-const divide = (n1, n2) => {
-  if (!(n1 / n2).isNaN) {
-    return n1 / n2;
-  }
-  return 'Error';
+const math = {
+  add(oper1, oper2) {
+    return Number(oper1) + Number(oper2);
+  },
+  subtract(oper1, oper2) {
+    return oper1 - oper2;
+  },
+  divide(oper1, oper2) {
+    return !(oper1 / oper2).isNaN ? "Error" : oper1 / oper2;
+  },
+  multiply(oper1, oper2) {
+    return oper1 * oper2;
+  },
 };
-const multiply = (n1, n2) => n1 * n2;
 
 const operate = (symbol, n1, n2) => {
   switch (symbol) {
-    case '+':
-      return add(n1, n2);
-    case '-':
-      return subtract(n1, n2);
-    case '/':
-      return divide(n1, n2);
-    case '*':
-      return multiply(n1, n2);
+    case "+":
+      return math.add(n1, n2);
+    case "-":
+      return math.subtract(n1, n2);
+    case "/":
+      return math.divide(n1, n2);
+    case "*":
+      return math.multiply(n1, n2);
   }
 };
 
 const resetDisplay = () => {
-  calcDisplay.textContent = '';
+  calcDisplay.textContent = "";
 };
 
 const clear = () => {
@@ -47,19 +53,19 @@ const clear = () => {
 const setOperator = (symbol) => {
   if (num1 !== null) {
     num2 = calcDisplay.innerHTML;
-    calcDisplay.textContent = Math.round(
-      (operate(operator, num1, num2) * 1000) / 1000,
-    );
+    // calcDisplay.textContent = Math.round(
+    //   (operate(operator, num1, num2) * 1000) / 1000
+    // );
     num1 = null;
     shouldResetDisplay = true;
   }
 
-  if (symbol === '+' || symbol === '-') {
+  if (symbol === "+" || symbol === "-") {
     operator = symbol;
-  } else if (symbol === '×') {
-    operator = '*';
-  } else if (symbol === '÷') {
-    operator = '/';
+  } else if (symbol === "×") {
+    operator = "*";
+  } else if (symbol === "÷") {
+    operator = "/";
   }
 
   shouldResetDisplay = true;
@@ -67,8 +73,8 @@ const setOperator = (symbol) => {
 };
 
 const appendDec = () => {
-  if (!calcDisplay.textContent.includes('.')) {
-    calcDisplay.textContent += '.';
+  if (!calcDisplay.textContent.includes(".")) {
+    calcDisplay.textContent += ".";
     shouldResetDisplay = false;
   }
 };
@@ -85,17 +91,21 @@ const calculate = () => {
   if (operator !== null) {
     num2 = calcDisplay.innerHTML;
     calcDisplay.textContent = Math.round(
-      (operate(operator, num1, num2) * 1000) / 1000,
+      (operate(operator, num1, num2) * 1000) / 1000
     );
   }
 };
 
-equals.addEventListener('click', calculate);
+equals.addEventListener("click", calculate);
+
 digits.forEach((digit) => {
-  digit.addEventListener('click', () => appendNum(digit.textContent));
+  digit.addEventListener("click", () => appendNum(digit.textContent));
 });
+
 operators.forEach((oper) => {
-  oper.addEventListener('click', () => setOperator(oper.innerHTML));
+  oper.addEventListener("click", () => setOperator(oper.innerHTML));
 });
-clearBtn.addEventListener('click', clear);
-decimal.addEventListener('click', appendDec);
+
+clearBtn.addEventListener("click", clear);
+
+decimal.addEventListener("click", appendDec);
